f378672edffe50c03d689b786e75b8e8f4be0c52
T-Long1207 <Longclan1@>
2022-06-09 16:10:19 +0700

    [Implement] Add web3dart

--- lib/ui/screens/auth/login_screen.dart
+++ lib/ui/screens/auth/login_screen.dart
@@ -1,5 +1,10 @@
+
+import 'package:flutter/services.dart';
+import 'package:http/http.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
+import 'package:web3dart/web3dart.dart';
 
 import '../../../core/viewmodels/language_provider.dart';
 
@@ -11,6 +16,51 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  late Client? httpClient;
+
+  late Web3Client? ethClient;
+
+//Ethereum address
+  final String myAddress = "0xbb0978f8970B5367EEE9dCe0C4200Fec1C90cdEe";
+
+//url from Infura
+  final String blockchainUrl =
+      "https://polygon-mumbai.g.alchemy.com/v2/SLD3j8ZWcKISkyYUmdWAOwruvTT5ovRI";
+
+//strore the value of alpha and beta
+  var totalVotesA;
+  var totalVotesB;
+
+  @override
+  void initState() {
+    httpClient = Client();
+    ethClient = Web3Client(blockchainUrl, httpClient!);
+    getTotalVotes();
+    super.initState();
+  }
+
+  Future<DeployedContract> getContract() async {
+    String abiFile = await rootBundle.loadString("assets/abi/contract.json");
+    String contractAddress = "0x790EA10a1991Bd4DfA2f698Aa78c58Fc453540A9";
+    final contract = DeployedContract(ContractAbi.fromJson(abiFile, "Voting"),
+        EthereumAddress.fromHex(contractAddress));
+
+    return contract;
+  }
+
+  Future<List<dynamic>> callFunction(String name) async {
+    final contract = await getContract();
+    final function = contract.function(name);
+    final result = await ethClient!.call(contract: contract, function: function, params: []);
+    return result;
+  }
+
+  Future<void> getTotalVotes() async {
+    List<dynamic> resultsA = await callFunction("name");
+    Logger().d(resultsA);
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Builder(builder: (context) {
@@ -19,9 +69,9 @@ class _LoginScreenState extends State<LoginScreen> {
         if (languageCollection.baseLanguageContentModel == null ||
             languageCollection.baseLanguageContentModel!.value == null) {
           languageCollection.getLanguageCollection(context);
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
+          // return const Center(
+          //   child: CircularProgressIndicator(),
+          // );
         }
         return Container();
       });
