2139bcdb318b54435a3d5226ab62365336741d30
Mrhealer <longkmttv9@gmail.com>
2022-07-16 19:21:15 +0700

    [Implement] add multi image for home

--- lib/ui/screens/splash/splash_screen.dart
+++ lib/ui/screens/splash/splash_screen.dart
@@ -39,7 +39,7 @@ class _SplashScreenState extends State<SplashScreen> {
         //       context, RouterGenerator.routeLogin);
         // }
         NavigatorUtils.pushReplaceRemoveAll(
-            context, RouterGenerator.routeDetailSwipeScreen);
+            context, RouterGenerator.routeHome);
       }
     });
   }
--- lib/ui/screens/widgets/swipe/profile_card.dart
+++ lib/ui/screens/widgets/swipe/profile_card.dart
@@ -1,32 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_dating_fi_mobile/ui/screens/detail/detail_swipe_screen.dart';
 import 'package:go_dating_fi_mobile/ui/screens/widgets/utils/colors.dart';
 import 'package:go_dating_fi_mobile/ui/screens/widgets/utils/text_style.dart';
 
 import '../../../../core/model/profile_model.dart';
 import '../utils/assets_utils.dart';
 
-class ProfileCard extends StatelessWidget {
+const duration = Duration(milliseconds: 300);
+
+class ProfileCard extends StatefulWidget {
   const ProfileCard({Key? key, required this.profile}) : super(key: key);
   final Profile profile;
 
   @override
+  State<ProfileCard> createState() => _ProfileCardState();
+}
+
+class _ProfileCardState extends State<ProfileCard> with RouteAware {
+  int activePage = 1;
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      height: 0.71.sh,
-      width: 0.9.sw,
-      padding: const EdgeInsets.symmetric(vertical: 10),
+      height: 0.72.sh,
+      width: 0.95.sw,
+      padding: const EdgeInsets.symmetric(vertical: 5),
       child: Stack(
         children: [
-          Positioned.fill(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.network(
-                profile.imageAsset1,
-                fit: BoxFit.fitHeight,
-              ),
-            ),
-          ),
+          ClipPath(
+              clipper: ShapeBorderClipper(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(24.r),
+                          topRight: Radius.circular(24.r),
+                          bottomLeft: Radius.circular(24.r),
+                          bottomRight: Radius.circular(24.r)))),
+              child: SizedBox(
+                  height: 0.7.sh,
+                  child: Stack(
+                    children: [
+                      PageView.builder(
+                          itemCount: 2,
+                          pageSnapping: true,
+                          // controller: _pageController,
+                          onPageChanged: (page) {
+                            // setState(() {
+                            //   activePage = page;
+                            // });
+                          },
+                          itemBuilder: (context, pagePosition) {
+                            //checking active position
+                            bool active = pagePosition == activePage;
+                            return slider([
+                              widget.profile.imageAsset1,
+                              widget.profile.imageAsset1
+                            ], pagePosition, active);
+                          }),
+                      Positioned.fill(
+                          bottom: 10.sm,
+                          child: Align(
+                              alignment: FractionalOffset.bottomCenter,
+                              child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: indicators(2, activePage))))
+                    ],
+                  ))),
           Positioned(
             bottom: 30.sm,
             child: Container(
@@ -44,57 +83,113 @@ class ProfileCard extends StatelessWidget {
                   ),
                 ],
               ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 20, bottom: 0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      profile.name,
-                      style: TextStyles.textSize34SWhite,
-                    ),
-                    Row(
-                      children: [
-                        Image.asset(AssetsUtils.ONLINE),
-                        SizedBox(
-                          width: 5.sm,
-                        ),
-                        Text(
-                          "Japan",
-                          style: TextStyles.text14RWhite,
-                        ),
-                        SizedBox(
-                          width: 5.sm,
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(4),
-                            color: Colors.transparent,
-                            border: Border.all(
-                              color: Colours.MAIN_COLOR,
+              child: InkWell(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20, bottom: 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        widget.profile.name,
+                        style: TextStyles.textSize34SWhite,
+                      ),
+                      Row(
+                        children: [
+                          Image.asset(AssetsUtils.ONLINE),
+                          SizedBox(
+                            width: 5.sm,
+                          ),
+                          Text(
+                            "Japan",
+                            style: TextStyles.text14RWhite,
+                          ),
+                          SizedBox(
+                            width: 5.sm,
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(4),
+                              color: Colors.transparent,
+                              border: Border.all(
+                                color: Colours.MAIN_COLOR,
+                              ),
+                            ),
+                            padding: const EdgeInsets.all(10),
+                            child: Text(widget.profile.distance,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                ),
+                                textAlign: TextAlign.center),
+                          )
+                        ],
+                      ),
+                      Text(
+                        '私は太陽の光です',
+                        style: TextStyles.textBold18RWhite,
+                      ),
+                    ],
+                  ),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    PageRouteBuilder(
+                      transitionsBuilder:
+                          (context, animation, secondaryAnimation, child) {
+                        return ScaleTransition(
+                          alignment: Alignment.center,
+                          scale: Tween<double>(begin: 0.1, end: 1).animate(
+                            CurvedAnimation(
+                              parent: animation,
+                              curve: Curves.bounceIn,
                             ),
                           ),
-                          padding: const EdgeInsets.all(10),
-                          child: Text(profile.distance,
-                              style: const TextStyle(
-                                color: Colors.white,
-                              ),
-                              textAlign: TextAlign.center),
-                        )
-                      ],
+                          child: child,
+                        );
+                      },
+                      transitionDuration: duration,
+                      pageBuilder: (BuildContext context,
+                          Animation<double> animation,
+                          Animation<double> secondaryAnimation) {
+                        return const DetailSwipeScreen();
+                      },
                     ),
-                    Text(
-                      '私は太陽の光です',
-                      style: TextStyles.textBold18RWhite,
-                    ),
-                  ],
-                ),
+                  );
+                },
               ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  AnimatedContainer slider(images, pagePosition, active) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOutCubic,
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: NetworkImage(
+                images[pagePosition],
+              ),
+              fit: BoxFit.cover)),
+    );
+  }
+
+  List<Widget> indicators(imagesLength, currentIndex) {
+    return List<Widget>.generate(imagesLength, (index) {
+      return Container(
+        margin: EdgeInsets.all(3.sm),
+        width: 7.w,
+        height: 7.h,
+        decoration: BoxDecoration(
+            color: currentIndex == index
+                ? Colours.MAIN_COLOR
+                : Colours.UN_SELECTED,
+            shape: BoxShape.circle),
+      );
+    });
   }
 }
